e5d60baf09fd42aabf0d25caea44a835acfc547e
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-04-19 11:49:28 -0300

    Exercício 002 do curso Python 3 Mundo 1

---
+++ ex-0002.py
@@ -0,0 +1,4 @@
+#DESAFIO 002
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+nome = input('Qual o seu nome? ')
+print('Bem-vindo {}, é um prazer te conhecer!'.format(nome))
