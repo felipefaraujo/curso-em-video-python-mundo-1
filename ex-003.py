9dd68309258403daa9acebe9d7ef78cc53cb6f92
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-04-19 21:06:05 -0300

    Exercício 003 do curso Python 3 Mundo 1

---
+++ ex-003.py
@@ -0,0 +1,6 @@
+#DESAFIO 003
+#Crie um programa que leia dois números inteiros e mostre a soma entre eles.
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+soma = n1 + n2
+print(f'A soma entre {n1} e {n2} é {soma}!')
